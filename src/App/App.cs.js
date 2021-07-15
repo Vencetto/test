bab356759aac7a85c124193176df4aceafc2d2bf
Volodymyr Zomber <Vencetto@users.noreply.github.com>
2021-07-15 13:16:16 +0300

    first steps

---
+++ src/App/App.cs.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+    background: #F1ECC3;
+    width: 100vw;
+    height: 100vh;
+`;
+
+export const ColContainer = styled.div`
+`;
---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import './App.cs.js';
+import * as SC from './App.cs';
+
+function App() {
+  return (
+    <SC.AppContainer>
+      
+    </SC.AppContainer>
+  );
+}
+
+export default App;
