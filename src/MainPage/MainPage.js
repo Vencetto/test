f7aec83eeffc714f9df435e9e9b3eb348be1063f
Volodymyr Zomber <Vencetto@users.noreply.github.com>
2021-07-15 15:50:40 +0300

    in progress sockets

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,22 +1,34 @@
+import React, { useEffect } from 'react';
 import { RaceItem } from '../RaceItem/RaceItem.js';
 import * as SC from './MainPage.sc';
 
-let arr = [ { id:1, name:"Frodo", race:"Hobbit" },
-            { id:2, name:"Aragorn", race:"Human" },
-            { id:3, name:"Gimli", race:"Dworf" },
-            { id:4, name:"Legolas", race:"Elf" }
-          ]
+const SERVER_URL = 'ws://testapi.marit.expert:3004'; 
+// const arr = [ { id:1, name:"Frodo", race:"Hobbit" },
+//             { id:2, name:"Aragorn", race:"Human" },
+//             { id:3, name:"Gimli", race:"Dworf" },
+//             { id:4, name:"Legolas", race:"Elf" }
+//           ]
 
 function MainPage() {
-  return (
-    <SC.MainContainer>
-      <SC.ColContainer>
-        {
-          arr.map(el => <RaceItem character={el} />)
-        }
-      </SC.ColContainer>
-    </SC.MainContainer>
-  )
+    const socket = new WebSocket(SERVER_URL);
+    let arr = [];
+
+    socket.onopen = function() {
+        socket.send({cmd:"get_list"});
+        socket.onmessage = (message) => { arr = message.data };
+    }
+
+    useEffect(() => console.log('here'), [arr]);
+
+    return (
+        <SC.MainContainer>
+            <SC.ColContainer>
+                {
+                    arr.map(el => <RaceItem character={el} id={el.id} />)
+                }
+            </SC.ColContainer>
+        </SC.MainContainer>
+    )
 }
 
 export default MainPage;
